727231dee8d371e1bd0e2a783d4ef6c0958d022d
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-09-20 01:03:19 -0500

    add ConstructorInjectedController class to demonstrate Constructor Injected Dependency manually as done by SpringFramework via IoC

---
+++ src/main/java/com/ernesto/springdi/controllers/ConstructorInjectedController.java
@@ -0,0 +1,16 @@
+package com.ernesto.springdi.controllers;
+
+import com.ernesto.springdi.services.GreetingService;
+
+public class ConstructorInjectedController {
+
+    private final GreetingService greetingService;
+
+    public ConstructorInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting() {
+        return greetingService.sayGreeting();
+    }
+}
