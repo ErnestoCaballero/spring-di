c447f083e1300d02a1f66fe3df9f5d31718090f1
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-09-20 01:25:45 -0500

    add spring stereotype annotations to SetterInjectedController class so that Spring add that bean to it's context and perform the Setter-based Dependency Injection. Including @Controller and @Autowired

--- src/main/java/com/ernesto/springdi/SpringDiApplication.java
+++ src/main/java/com/ernesto/springdi/SpringDiApplication.java
@@ -2,6 +2,7 @@ package com.ernesto.springdi;
 
 import com.ernesto.springdi.controllers.MyController;
 import com.ernesto.springdi.controllers.PropertyInjectedController;
+import com.ernesto.springdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -18,13 +19,14 @@ public class SpringDiApplication {
 		String greeting = myController.sayHello();
 		System.out.printf("%s from SpringDiApplication.java\n", greeting);
 
-		System.out.println("------ Property");
-
+		System.out.println("------ Property-based");
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
 		System.out.println(propertyInjectedController.getGreeting());
 
 		// Setter-based dependency injection
-		
+		System.out.println("------ Setter-based");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
 	}
 
 }
