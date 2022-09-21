228de26c71ebf3630fe209b394209783bd5343ca
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-09-20 23:46:51 -0500

    change implementation of MyController to a Constructor-based injected dependency that will default to use the primary bean

--- src/main/java/com/ernesto/springdi/controllers/MyController.java
+++ src/main/java/com/ernesto/springdi/controllers/MyController.java
@@ -1,13 +1,18 @@
 package com.ernesto.springdi.controllers;
 
+import com.ernesto.springdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
-    public String sayHello() {
-        System.out.println("Entering sayHello() method from MyController.java");
+    private final GreetingService greetingService;
 
-        return "Hello, world!";
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello() {
+        return greetingService.sayGreeting();
     }
 }
