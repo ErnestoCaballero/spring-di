d6eb04c786a97f3e6d98f211468c907ab73da71c
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-09-28 01:04:07 -0500

    change I18nGreeting Services and Primary Bean into the Java Configuration Implementation on the GreetingService config class

--- src/main/java/com/ernesto/springdi/config/GreetingServiceConfig.java
+++ src/main/java/com/ernesto/springdi/config/GreetingServiceConfig.java
@@ -1,13 +1,31 @@
 package com.ernesto.springdi.config;
 
-import com.ernesto.springdi.services.ConstructorGreetingService;
-import com.ernesto.springdi.services.PropertyInjectedGreetingService;
-import com.ernesto.springdi.services.SetterInjectedGreetingService;
+import com.ernesto.springdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService() {
+        return new PrimaryGreetingService();
+    }
+
+    @Profile({"ES", "default"})
+    @Bean
+    I18nSpanishGreetingService i18nService() {
+        return new I18nSpanishGreetingService();
+    }
+
+    @Profile("EN")
+    @Bean("i18nService")
+    I18nEnglishGreetingService i18nEnglishGreetingService() {
+        return new I18nEnglishGreetingService();
+    }
 
     @Bean
     ConstructorGreetingService constructorGreetingService() {
--- src/main/java/com/ernesto/springdi/services/PrimaryGreetingService.java
+++ src/main/java/com/ernesto/springdi/services/PrimaryGreetingService.java
@@ -3,8 +3,6 @@ package com.ernesto.springdi.services;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
-@Primary
-@Service
 public class PrimaryGreetingService implements GreetingService {
 
     @Override
