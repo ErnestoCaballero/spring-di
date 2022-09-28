781a2c4ac2c2c0ff36581f6e9458e35c4f8559d8
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-09-28 00:33:51 -0500

    change the Greeting Services into a Java Configuration class
    we made this by creating a new package config and inside putting the GreetingServiceConfig class
    which use the @Configuration annotation to let spring now that it should scan that class for beans
    as well as the @Bean annotation to let spring context load the methods that return the objects as later
    to be used as beans

---
+++ src/main/java/com/ernesto/springdi/config/GreetingServiceConfig.java
@@ -0,0 +1,26 @@
+package com.ernesto.springdi.config;
+
+import com.ernesto.springdi.services.ConstructorGreetingService;
+import com.ernesto.springdi.services.PropertyInjectedGreetingService;
+import com.ernesto.springdi.services.SetterInjectedGreetingService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GreetingServiceConfig {
+
+    @Bean
+    ConstructorGreetingService constructorGreetingService() {
+        return new ConstructorGreetingService();
+    }
+
+    @Bean
+    PropertyInjectedGreetingService propertyInjectedGreetingService() {
+        return new PropertyInjectedGreetingService();
+    }
+
+    @Bean
+    SetterInjectedGreetingService setterInjectedGreetingService() {
+        return new SetterInjectedGreetingService();
+    }
+}
