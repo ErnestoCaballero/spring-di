cec3c12aa16c8ae68d29566ece6812085fee6342
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-09-28 01:32:25 -0500

    use dependency injection inside java configuration
    create a new package repositories emulating the case of a DB management
    inside create an EnglishGreetingRepository interface with getGreeting() and then its implementation
    EnglishGreetingRepositoryImpl implementing the method and returning a String
    Then change the I18nEnglishGreetingService implementation by declaring a EnglishGreetingRepository field into the class
    create a constructor that needs to initialize that field
    finally, go GreetingServiceConfig and declare a Bean for EnglishGreetingRepository returning the implementation
    and using it to be injected into the I18nEnglishGreetingService bean

---
+++ src/main/java/com/ernesto/springdi/repositories/EnglishGreetingRepository.java
@@ -0,0 +1,6 @@
+package com.ernesto.springdi.repositories;
+
+public interface EnglishGreetingRepository {
+
+    String getGreeting();
+}
--- src/main/java/com/ernesto/springdi/services/I18nEnglishGreetingService.java
+++ src/main/java/com/ernesto/springdi/services/I18nEnglishGreetingService.java
@@ -1,10 +1,17 @@
 package com.ernesto.springdi.services;
 
+import com.ernesto.springdi.repositories.EnglishGreetingRepository;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 //@Service("i18nService")
 public class I18nEnglishGreetingService implements GreetingService {
+
+    private final EnglishGreetingRepository englishGreetingRepository;
+
+    public I18nEnglishGreetingService(EnglishGreetingRepository englishGreetingRepository) {
+        this.englishGreetingRepository = englishGreetingRepository;
+    }
 
     @Override
     public String sayGreeting() {
