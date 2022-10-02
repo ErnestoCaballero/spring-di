c7b2255845aa735e2b7fa263cac8b35f89bf1a40
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-10-02 02:27:06 -0500

    move connection values to application.properties
    taking advantage that SpringBoot loads into the context whatever is
    in the application.properties file

--- src/main/java/com/ernesto/springdi/config/GreetingServiceConfig.java
+++ src/main/java/com/ernesto/springdi/config/GreetingServiceConfig.java
@@ -9,7 +9,6 @@ import guru.spring.pets.PetServiceFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
-@PropertySource("classpath:datasource.properties")
 @ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
